b13235df6f6c80db0ece78e323a12c63b7cb3d42
NetanelYaso <netanelyaso12@gmail.com>
2023-01-03 10:19:58 +0200

    creating header footer and home components

---
+++ client/src/components/featurs/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="header">
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default template;
---
+++ client/src/components/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="home">
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default template;
