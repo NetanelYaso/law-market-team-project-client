59f8d15e9c8ced7222cc7c57ded2768287f96b2e
NetanelYaso <netanelyaso12@gmail.com>
2023-01-04 16:28:30 +0200

    commit 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import ProfileCard from './components/featurs/lawyerProfile/ProfileCard/ProfileCard';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Header from './components/featurs/Header/Header';
 import Footer from './components/featurs/Footer/Footer';
@@ -9,7 +8,6 @@ function App() {
     <div>
       <BrowserRouter>
         <Header />
-        <ProfileCard />
         <Footer />
       </BrowserRouter>
     </div>
