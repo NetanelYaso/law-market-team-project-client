18fd0e961e6aa5275941a4e62aac5d4354feb470
DANIELGEDAMO <danielgadamo@gmail.com>
2023-01-04 17:23:10 +0200

    o

--- client/src/components/featurs/home Page featurs/card/HomeCardPage.jsx
+++ client/src/components/featurs/home Page featurs/card/HomeCardPage.jsx
@@ -3,16 +3,7 @@ import "../card/HomeCardPage.css";
 
 export default function HomeCardPage() {
   return (
-    // <Card sx={{ maxWidth: 345 }}>
-    //   <CardActionArea className='card-for-home-page'>
-    //     <CardMedia
-    //       height="200"
-    //     />
-    //     <div className='textInCard'>CATEGORY</div>
-    //   </CardActionArea>
-    //   <CardActions>
-    //   </CardActions>
-    // </Card>
+
     <container className = "cardsContainer">
     <div className="card-for-home-page">
       <div className="textInCard"> CATEGORY</div>
