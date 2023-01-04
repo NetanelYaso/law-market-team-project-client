51d48c1f0a3a993f352298bc0dbd44675fe56bc6
lior2911 <aytegev1@gmail.com>
2023-01-04 10:44:16 +0200

    uik

--- client/src/components/pages/LawyerProfile/LawyerProfile.jsx
+++ client/src/components/pages/LawyerProfile/LawyerProfile.jsx
@@ -5,7 +5,6 @@ import TitleCard from '../../featurs/lawyerProfile/TitleCard/TitleCard'
 const LawyerProfile = () => {
   return (
     <div className='lawyerContainer'>
-      <TitleCard/>
       <ProfileCard/>
     </div>
   )
