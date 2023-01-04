07476e479a5f9ded50167754fbbb465407bd179e
OrGadamo <brber223@gmail.com>
2023-01-04 19:54:22 +0200

    header footer - complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import ProfileCard from './components/featurs/lawyerProfile/ProfileCard/ProfileCard';
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Header from './components/featurs/Header/Header';
-import Footer from './components/featurs/Footer/Footer';
+import "./App.css";
+// import ProfileCard from './components/featurs/lawyerProfile/ProfileCard/ProfileCard';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/featurs/Header/Header";
+import Footer from "./components/featurs/Footer/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
-        <ProfileCard />
+        {/* <ProfileCard /> */}
         <Footer />
       </BrowserRouter>
     </div>
   );
 }
-;
 export default App;
