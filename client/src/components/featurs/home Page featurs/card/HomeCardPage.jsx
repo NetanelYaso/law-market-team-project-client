f8382ccad5ad841b643fe8a68b25aeb338eebf2e
DANIELGEDAMO <danielgadamo@gmail.com>
2023-01-06 18:10:38 +0200

    cc

--- client/src/components/featurs/home Page featurs/card/HomeCardPage.jsx
+++ client/src/components/featurs/home Page featurs/card/HomeCardPage.jsx
@@ -7,23 +7,35 @@ import {
   MDBModalContent,
   MDBModalTitle,
   MDBModalFooter,
-} from "mdb-react-ui-kit";
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardOverlay,
+  MDBCardImage
+} from 'mdb-react-ui-kit';
 
 export default function HomeCardPage() {
   const [centredModal, setCentredModal] = useState(false);
 
   const toggleShow = () => setCentredModal(!centredModal);
   return (
-    <>
-      <MDBModalTitle className=" card-for-home-page " onClick={toggleShow}>
-        <div className="textInCard">CATEGORY</div>
+      <>
+    <MDBCard background='dark' className='text-white imagStyle'>
+      <MDBCardImage className="imag" overlay src='https://mdbootstrap.com/img/new/slides/017.webp' alt='...' />
+      <MDBCardOverlay className= "boxTitel myText ">
+        <MDBCardTitle className="textInCard" onClick={toggleShow}>Card title</MDBCardTitle>
+
+      </MDBCardOverlay>
+    </MDBCard>
+
+      <MDBModalTitle className=" card-for-home-page " >
       </MDBModalTitle>
 
       <MDBModal tabIndex="-1" show={centredModal} setShow={setCentredModal}>
         <MDBModalDialog centered>
           <MDBModalContent>
             <figure className="PopUpModel">
-              <div className="textInModle">CATEGORY</div>
+               <div className="textInModle">Card title</div> 
             </figure>
             <ul className="cardul">
               <li className="cardLi">kkkkkkkk</li>
