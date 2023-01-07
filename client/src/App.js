30a498442e8bcf857c0d883ae8643b16a11a0a0a
NetanelYaso <netanelyaso12@gmail.com>
2023-01-07 19:17:57 +0200

    creatnig services for the fetching and generic cards

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,16 @@ import ProfileCard from "./components/featurs/lawyerProfile/ProfileCard/ProfileC
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/featurs/Header/Header";
 import Footer from "./components/featurs/Footer/Footer";
+import GenericCard from "./components/featurs/GenericCard/GenericCard";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <HomeCardPage />
+        {/* <HomeCardPage /> */}
         {/* <ProfileCard /> */}
+        <GenericCard/>
         <Footer />
       </BrowserRouter>
     </div>
---
+++ client/src/components/pages/SubDepartments/SubDepartments.test.js
@@ -0,0 +1,10 @@
+import React    from "react";
+import template from "./SubDepartments.test.js";
+
+class SubDepartments extends React.Component {
+  render() {
+    return template.call(this);
+  }
+}
+
+export default SubDepartments;
