0f55c7ae280f5607ef0993a78a95844a1780cbab
lior2911 <aytegev1@gmail.com>
2023-01-11 20:29:40 +0200

    login

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import {UserAuthProvider} from './context/googleAuth'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <UserAuthProvider>
     <App />
+    </UserAuthProvider>
   </React.StrictMode>
 );
 
