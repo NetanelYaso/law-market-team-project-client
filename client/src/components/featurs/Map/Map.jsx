b3b9d0cdd97696dd4939135f812a5470b173220d
OrGadamo <brber223@gmail.com>
2023-01-11 04:50:19 +0200

    map

---
+++ client/src/components/featurs/Map/Map.jsx
@@ -0,0 +1,128 @@
+import "./Map.css";
+import {
+  MDBContainer,
+  MDBIcon,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+} from "mdb-react-ui-kit";
+import MapInfoWindow from "../MapInfoWindow/MapInfoWindow";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  MarkerF,
+  InfoWindowF,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { useState } from "react";
+// const GOOGLE_MAPS_API_KEY = 'AIzaSyBuymyezM5emJbmNyCmLLr3bLL7U3oMIu0'
+const center = { lat: 31.949181, lng: 34.893261 };
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyBuymyezM5emJbmNyCmLLr3bLL7U3oMIu0",
+  });
+  const [map, setMap] = useState(null);
+  const [infoWindowVisible, setInfoWindowVisible] = useState(false);
+  const [directionResponse, setDirectionResponse] = useState(null);
+  const [duration, setDuration] = useState("");
+  const [distance, setDistance] = useState("");
+  if (!isLoaded) {
+    return <div>Map cannot be loaded right now</div>;
+  }
+  async function calculateRoute(userPosition) {
+    if (!userPosition) return;
+    console.log(userPosition, center);
+    const directionService = new window.google.maps.DirectionsService();
+    const result = await directionService.route({
+      origin: userPosition,
+      destination: center,
+      travelMode: "DRIVING",
+      provideRouteAlternatives: false,
+    });
+    setDirectionResponse(result);
+    setDistance(result.routes[0].legs[0].distance.text);
+    setDuration(result.routes[0].legs[0].duration.text);
+  }
+  const getPosition = () => {
+    return new Promise((res, rej) => {
+      navigator.geolocation.getCurrentPosition(res, rej);
+    });
+  };
+  const getCurrentLocation = async () => {
+    const position = await getPosition();
+    return { lat: position.coords.latitude, lng: position.coords.longitude };
+  };
+  return (
+    <>
+      <MDBContainer className="position-relative" id="map_container">
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map) => setMap(map)}
+        >
+          <MarkerF
+            position={center}
+            onClick={() => setInfoWindowVisible(!infoWindowVisible)}
+          />
+          {directionResponse && (
+            <DirectionsRenderer directions={directionResponse} />
+          )}
+          {infoWindowVisible && (
+            <InfoWindowF
+              id="infowindow_con"
+              position={center}
+              onCloseClick={() => setInfoWindowVisible(!infoWindowVisible)}
+            >
+              <MDBContainer>
+                <MapInfoWindow />
+              </MDBContainer>
+            </InfoWindowF>
+          )}
+        </GoogleMap>
+        <MDBContainer
+          id="on_map_elements"
+          className="position-absolute bottom-0 start-50 w-50"
+        >
+          <MDBCard>
+            <MDBCardBody>
+              <MDBCardTitle>Direction Information</MDBCardTitle>
+              <div className="d-flex justify-content-between">
+                <span>Distance: {distance}</span>
+                <span>Duration: {duration}</span>
+                <MDBBtn
+                  className="me-1"
+                  color="danger"
+                  onClick={async () => {
+                    getCurrentLocation().then((location) =>
+                      calculateRoute(location)
+                    );
+                  }}
+                >
+                  Calculate Route
+                </MDBBtn>
+              </div>
+              <MDBBtn
+                className="mx-1 position-absolute top-0 end-0"
+                id="location-arrow"
+                tag="a"
+                color="secondry"
+                floating
+                onClick={() => map.panTo(center)}
+              >
+                <MDBIcon fas icon="location-arrow" />
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBContainer>
+      </MDBContainer>
+    </>
+  );
+}
+
+export default Map;
--- client/src/components/featurs/lawyerProfile/ProfileCard/ProfileCard.jsx
+++ client/src/components/featurs/lawyerProfile/ProfileCard/ProfileCard.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+// import React from "react";
+// import Card from "@mui/material/Card";
+// import CardActions from "@mui/material/CardActions";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
 
-const ProfileCard = () => {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-        <CardMedia
-        component="img"
-        alt="green iguana"
-        height="40"
-        width='40'
-        image="client\public\logo192.png"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-  );
-}
+// const ProfileCard = () => {
+//   return "";
+//   // <Card sx={{ maxWidth: 345 }}>
+//   //   <CardMedia
+//   //     component="img"
+//   //     alt="green iguana"
+//   //     height="140"
+//   //     image="/static/images/cards/contemplative-reptile.jpg"
+//   //   />
+//   //     <CardMedia
+//   //     component="img"
+//   //     alt="green iguana"
+//   //     height="40"
+//   //     width='40'
+//   //     image="client\public\logo192.png"
+//   //   />
+//   //   <CardContent>
+//   //     <Typography gutterBottom variant="h5" component="div">
+//   //       Lizard
+//   //     </Typography>
+//   //     <Typography variant="body2" color="text.secondary">
+//   //       Lizards are a widespread group of squamate reptiles, with over 6,000
+//   //       species, ranging across all continents except Antarctica
+//   //     </Typography>
+//   //   </CardContent>
+//   //   <CardActions>
+//   //     <Button size="small">Share</Button>
+//   //     <Button size="small">Learn More</Button>
+//   //   </CardActions>
+//   // </Card>
+//   // );
+// };
 
-export default ProfileCard
+// export default ProfileCard;
