16649baab0dd5a526e12bb7300d011ef7000e7a0
NetanelYaso <netanelyaso12@gmail.com>
2023-01-11 23:42:44 +0200

    Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,15 @@
 import "./App.css";
-// import Home from "./components/pages/Home/HomePageCard"
-import HomeCardPage from "./components/featurs/home Page featurs/card/main-cards/HomeCardPage";
-import ProfileCard from "./components/featurs/lawyerProfile/ProfileCard/ProfileCard";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/featurs/Header/Header";
 import Footer from "./components/featurs/Footer/Footer";
-import GenericCard from "./components/featurs/GenericCard/GenericCard";
-import LawyerProfile from "./components/pages/LawyerProfile/LawyerProfile";
-// import BottomCard from "./components/featurs/home Page featurs/card/bottom-cards/BottomCard";
-import SubCategory from "./components/pages/SubCategory/SubCategory";
-import DetailsCard from './components/featurs/payment/DetailsCard/index';
+import Router from "./Router";
 function App() {
-  // console.log(JSON.parse(localStorage.getItem("details"))) 
-  // console.log(JSON.parse(localStorage.getItem("inputs"))) 
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        {/* <HomeCardPage /> */}
-        <DetailsCard/>
-        {/* <BottomCard/> */}
-        {/* <ProfileCard /> */}
-        {/* <SubCategory/> */}
+        <Router />
         <Footer />
       </BrowserRouter>
     </div>
