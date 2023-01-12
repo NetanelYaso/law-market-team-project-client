88582969c4846589d422a210e011ff69c051a6d8
NetanelYaso <netanelyaso12@gmail.com>
2023-01-12 11:32:10 +0200

    adding button to generic card

--- client/src/components/featurs/GenericCard/GenericCard.jsx
+++ client/src/components/featurs/GenericCard/GenericCard.jsx
@@ -1,35 +1,36 @@
 import "./GenericCard.css";
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Button } from 'react-bootstrap';
 
 function GenericCard({ name, discription }) {
 
-  
+
 
   return (
     <div className="generiCard_div">
-        <Card className="first_card" style={{ width: '15rem', height: '10rem',zIndex:-1 }}>
-          <Card.Body className="genericCard">
-            {/* <Card.Title></Card.Title> */}
-            <Card.Title>
-              {name}
-            </Card.Title>
-            <Card.Text>
-              <ul>
-                <li>{discription[0]}</li>
-                <li>{discription[1]}</li>
-                <li>{discription[2]}</li>
-              </ul>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        {/* <Card className="second_card" style={{ width: '15rem', height: '10rem' }} >
-          <Card.Body className="genericCard">
-            <Card.Title><i className="far fa-heart"></i></Card.Title>
-            <Card.Title>
-              {name}
-            </Card.Title>
-          </Card.Body>
-        </Card> */}
+      <Card className="first_card" style={{ width: '15rem', height: '10rem', zIndex: -1 }}>
+        <Card.Body className="genericCard">
+          {/* <Card.Title></Card.Title> */}
+          <Card.Title>
+            {name}
+          </Card.Title>
+          <Card.Text>
+            <ul>
+              <li>{discription[0]}</li>
+              <li>{discription[1]}</li>
+              <li>{discription[2]}</li>
+            </ul>
+          </Card.Text>
+          <Button onClick={() => { }}>גלה עוד</Button>
+        </Card.Body>
+      </Card>
+      {/* <Card className="second_card" style={{ width: '15rem', height: '10rem' }} >
+        <Card.Body className="genericCard">
+          <Card.Title><i className="far fa-heart"></i></Card.Title>
+          <Card.Title>
+            {name}
+          </Card.Title>
+        </Card.Body>
+      </Card> */}
     </div>
   );
 }
