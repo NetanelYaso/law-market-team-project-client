035bf47883d0f099a8e83e43ca79dc2202e23495
NetanelYaso <netanelyaso12@gmail.com>
2023-01-15 13:49:57 +0200

    c

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import ProfileCard from "./components/featurs/lawyerProfile/ProfileCard/ProfileCard"
-// import PaymentCard from "./components/featurs/payment/PaymentCard/PaymentCard"
 import HomePage from "./components/pages/Home/HomePage"
 import PageNotFound from "./components/pages/PageNotFound/PageNotFound"
 import SubCategory from "./components/pages/SubCategory/SubCategory"
 import Login from "./components/pages/Login/Login"
 import SignUp from "./components/pages/SignUp/SignUp"
+import DetailsCard from "./components/featurs/payment/DetailsCard/DetailsCard"
 
 const Router = () => {
     return (
@@ -15,7 +15,7 @@ const Router = () => {
             <Route path="/profileCard" element={<ProfileCard />} />
             <Route path="/signUp" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/payment" element={<PaymentCard/> } /> */}
+            <Route path="/payment" element={<DetailsCard/> } />
             <Route path="*" element={<PageNotFound/> } />
         </Routes>
     )
