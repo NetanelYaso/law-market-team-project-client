b0c234f6b9a3d18e4553da23cd911ce36f4548da
DANIELGEDAMO <danielgadamo@gmail.com>
2023-01-16 23:40:41 +0200

    home page

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,13 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import ProfileCard from "./components/featurs/lawyerProfile/ProfileCard/ProfileCard"
 import AdminEditor from "./components/pages/AdminEditor/AdminDataEdit.jsx"
-import HomePage from "./components/pages/Home/homePage"
+import HomePage from "./components/pages/Home/HomePage"
 import PageNotFound from "./components/pages/PageNotFound/PageNotFound"
 import SubCategory from "./components/pages/SubCategory/SubCategory"
 import Login from "./components/pages/Login/Login"
 import SignUp from "./components/pages/SignUp/SignUp"
 import DetailsCard from './components/featurs/payment/DetailsCard/index';
-import HomePage from './components/pages/Home/homePage'
 import LawyersSelection from "./components/pages/LawyersSelection/LawyersSelection"
 import AboutUs from "./components/pages/AboutUs/AboutUs"
 
