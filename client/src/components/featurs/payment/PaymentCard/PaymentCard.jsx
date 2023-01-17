197f21bd834d9bd2d591f6e59d0c3836ba7777ad
abayasaye <abay400@gmail.com>
2023-01-18 01:43:14 +0200

    MERGE TO DEV

--- client/src/components/featurs/payment/PaymentCard/PaymentCard.jsx
+++ client/src/components/featurs/payment/PaymentCard/PaymentCard.jsx
@@ -21,7 +21,7 @@ import {
   MDBModalFooter,
 
 } from 'mdb-react-ui-kit';
-import ConfirmPayment from '../confirmPayment/ConfirmPayment';
+import ConfirmPayment from '../ConfirmPayment/ConfirmPayment';
 import { useNavigate } from "react-router-dom";
 
 const PaymentCard = ()=> {
